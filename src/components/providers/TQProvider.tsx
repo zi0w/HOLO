866c5641ed815f4f448b39809ae9ad29b22a7dbd
minji7901 <kimem7901@gmail.com>
2025-01-06 16:20:17 +0900

    chore: 초기셋팅

---
+++ src/components/providers/TQProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
+
+const TQProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default TQProvider;
