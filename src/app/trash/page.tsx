093a8d561da27d73d5e26180bb4d7976abdb33dd
minji7901 <kimem7901@gmail.com>
2025-01-07 10:13:13 +0900

    chore: 불필요한 파일 삭제,  지역 목데이터 생성

---
+++ src/app/trash/page.tsx
@@ -0,0 +1,13 @@
+const page = () => {
+  return (
+    <div>
+      <div>
+        <h2>쓰레기 가이드</h2>
+        <p>우리 동네 쓰레기 배출 일정과 분리배출 정보</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default page;
