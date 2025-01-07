bfddaed8806555b24acf6d293ba05d758ff503b1
zi0w <zzz13588@naver.com>
2025-01-07 20:11:55 +0900

    HOLO-2 <feat: 응답 선택시 사용할 funnel 훅 구현>

---
+++ src/app/recommend/_hooks/useFunnel.tsx
@@ -0,0 +1,54 @@
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState, type ReactElement, type ReactNode } from "react";
+
+type StepProps = {
+  name: string;
+  children: ReactNode;
+};
+
+type FunnelProps = {
+  children: Array<ReactElement<StepProps>>;
+};
+
+export const useFunnel = (initialStep: string) => {
+  const searchParams = useSearchParams();
+  // 초기 상태 설정
+  const [currentStep, setCurrentStep] = useState<string>(
+    () => searchParams.get("step") || initialStep,
+  );
+
+  // 상태와 url 동기화
+  useEffect(() => {
+    const stepParam = searchParams.get("step");
+    if (stepParam) {
+      setCurrentStep(stepParam);
+    } else {
+      window.history.replaceState(null, "", `?step=${currentStep}`);
+    }
+  }, [searchParams]);
+
+  const Step = ({ name, children }: StepProps): ReactNode => {
+    return <>{children}</>;
+  };
+
+  const Funnel = ({ children }: FunnelProps) => {
+    const steps = children.filter((child) => child.type === Step);
+    const activeStep = steps.find((child) => child.props.name === currentStep);
+    return activeStep || null;
+  };
+
+  const updateStep = (step: string): void => {
+    setCurrentStep(step);
+    window.history.pushState(null, "", `?step=${step}`);
+  };
+
+  const next = (nextStep: string): void => {
+    updateStep(nextStep);
+  };
+
+  const prev = (prevStep: string): void => {
+    updateStep(prevStep);
+  };
+
+  return { Funnel, Step, next, prev };
+};
