f7a38f00262bf898df552d78d16fddec2d755bea
Eunhye Jeong <gracejelly125@gmail.com>
2025-01-08 14:00:24 +0900

    HOLO-35 <feat: 포스트 업데이트 기능 구현>

---
+++ src/app/honeytips/_actions/update.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { createClient } from "@/lib/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import type { Post } from "../_types/honeytips.type";
+
+type updatePostProps = {
+  updatedTitle: Post["title"];
+  updatedContent: Post["content"];
+  updatedPostImageUrl: Post["post_image_url"];
+  updatedCategory: Post["categories"];
+  postId: Post["id"];
+};
+
+export const updatePost = async ({
+  postId,
+  updatedTitle,
+  updatedContent,
+  updatedPostImageUrl,
+  updatedCategory,
+}: updatePostProps) => {
+  const supabase = await createClient();
+  const user_id = "9826a705-38ce-4a07-b0dc-cbfb251355e3";
+  await supabase
+    .from("posts")
+    .update({
+      title: updatedTitle,
+      content: updatedContent,
+      post_image_url: updatedPostImageUrl,
+      categories: updatedCategory,
+      user_id: user_id,
+    })
+    .eq("id", postId)
+    .select();
+
+  revalidatePath(`/honeytips/${postId}`);
+  redirect(`/honeytips/${postId}`);
+};
