3137b4664b795f39b5e214b15a5b50e718cfcd46
신상용 <kurt-hide@sinsang-yong-ui-MacBookAir.local>
2025-01-17 20:33:48 +0900

    HOLO-66 <fix: 타입에러 수정>

--- src/app/mypage/_utils/MyfetchLikePostsData.ts
+++ src/app/mypage/_utils/MyfetchLikePostsData.ts
@@ -1,20 +1,20 @@
-// import { createClient } from "@/lib/utils/supabase/client";
+import { createClient } from "@/lib/utils/supabase/client";
 
-// const supabase = createClient();
+const supabase = createClient();
 
-// // Fetch all posts with their likes and user information
-// export const MyfetchLikePostsData = async () => {
-//   const { data, error } = await supabase
-//     .from("posts")
-//     .select("*, likes(user_id), users(nickname, profile_image_url), post_image_url") // Join users table to get nickname and profile image URL
-//     .order("created_at", { ascending: false }); // Sort by creation time
+// Fetch all posts with their likes and user information
+export const MyfetchLikePostsData = async () => {
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*, likes(user_id), users(nickname, profile_image_url), post_image_url") // Join users table to get nickname and profile image URL
+    .order("created_at", { ascending: false }); // Sort by creation time
 
-//   if (error) {
+  if (error) {
    
-//     throw error;
-//   }
+    throw error;
+  }
 
  
 
-//   return data || [];
-// };
+  return data || [];
+};
