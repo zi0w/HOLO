46503f66d94b660ce99e9f519a070f342189c21f
minji7901 <kimem7901@gmail.com>
2025-01-19 00:45:37 +0900

    HOLO-26 <feat: 작은 로딩추가, 페이지네이션 스타일 수정>

---
+++ src/components/common/LoadingSmall.tsx
@@ -0,0 +1,7 @@
+const LoadingSmall = () => {
+  return (
+    <div className="mx-auto h-7 w-7 animate-spin rounded-full border-4 border-white border-t-primary-500"></div>
+  );
+};
+
+export default LoadingSmall;
