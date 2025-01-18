03b9436112abb80c3f1145e78d97729360a7cb8a
minji7901 <kimem7901@gmail.com>
2025-01-19 00:43:43 +0900

    HOLO-26 <chore: 메인 배너 이름 변경 및 gmarketsansMedium 변경>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import MainHeader from "@/components/common/MainHeader";
 import BestPostList from "@/components/main/BestPostList";
-import PolicyAndTrashBnr from "@/components/main/PolicyAndTrashBnr";
+import PolicyAndTrashBanner from "@/components/main/PolicyAndTrashBanner";
 
-import MainBnr from "@/components/main/MainBnr";
+import MainBanner from "@/components/main/MainBanner";
 import Map from "@/components/main/Map";
 
 const Home = () => {
   return (
     <div className="mt-[70px]">
       <MainHeader />
-      <MainBnr />
+      <MainBanner />
       <Map />
       {/* 정책 배너 */}
-      <PolicyAndTrashBnr isPolicy={true} />
+      <PolicyAndTrashBanner isPolicy={true} />
       {/* 쓰레기 배너 */}
-      <PolicyAndTrashBnr isPolicy={false} />
+      <PolicyAndTrashBanner isPolicy={false} />
       <BestPostList />
     </div>
   );
