2b27ffbe40c148b5ab67098942469072ef70d6f5
minji7901 <kimem7901@gmail.com>
2025-01-27 13:58:33 +0900

    HOLO-204 <style: pc ui 적용>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import MainHeader from "@/components/common/MainHeader";
 import BestPostList from "@/components/main/BestPostList";
 import PolicyAndTrashBanner from "@/components/main/PolicyAndTrashBanner";
 
+import DailyFortune from "@/components/main/DailyFortune";
+import DailyMenu from "@/components/main/DailyMenu";
 import MainBanner from "@/components/main/MainBanner";
 import Map from "@/components/main/Map";
-import DailyMenu from "@/components/main/DailyMenu";
-import DailyFortune from "@/components/main/DailyFortune";
 
 const Home = () => {
   return (
     <div className="mt-[70px]">
-      <MainHeader />
+      <div className="lg:hidden">
+        <MainHeader />
+      </div>
       <MainBanner />
       <Map />
       {/* 정책 배너 */}
