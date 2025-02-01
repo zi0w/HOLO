e2adbc98f4c79ee90ebf57e74fbbea208f7d180c
zi0w <zzz13588@naver.com>
2025-02-01 19:54:05 +0900

    HOLO-203 <feat: 포츈쿠키 컴포넌트 분리 + 애니메이션 효과 적용>

--- src/app/fortune/page.tsx
+++ src/app/fortune/page.tsx
@@ -1,12 +1,5 @@
-import Fortune1 from "@/assets/images/fortune/fortune1.png";
-import Fortune2 from "@/assets/images/fortune/fortune2.png";
-import Fortune3 from "@/assets/images/fortune/fortune3.png";
-import Fortune4 from "@/assets/images/fortune/fortune4.png";
-import Fortune5 from "@/assets/images/fortune/fortune5.png";
-import Fortune6 from "@/assets/images/fortune/fortune6.png";
+import FortuneItem from "@/app/fortune/_components/FortuneItem";
 import type { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "오늘의 운세 뽑기",
@@ -19,19 +12,10 @@ export const metadata: Metadata = {
   },
 };
 
-const fortuneImages = [
-  Fortune1,
-  Fortune2,
-  Fortune3,
-  Fortune4,
-  Fortune5,
-  Fortune6,
-];
-
 const fortunePage = () => {
   return (
     <div className="mt-4 flex h-full flex-col items-center bg-white">
-      <h1 className="text-center mt-20 text-2xl text-base-800">
+      <h1 className="mt-20 text-center text-2xl text-base-800">
         오늘의 운세를 <br />
         알려줄 포춘쿠키는?
       </h1>
@@ -39,17 +23,7 @@ const fortunePage = () => {
         직감적으로! 하나만 골라보세요 :)
       </span>
       <div className="mt-10 grid grid-cols-2 gap-4">
-        {fortuneImages.map((src, index) => (
-          <Link href="/fortune/result" key={index}>
-            <Image
-              src={src}
-              alt={`fortune-${index + 1}`}
-              width={160}
-              height={160}
-              className="rounded-full hover:bg-[#ffd1a3]"
-            />
-          </Link>
-        ))}
+        <FortuneItem />
       </div>
     </div>
   );
