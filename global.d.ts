500c36acb4480690c38b8310fef15647c55013b6
zi0w <zzz13588@naver.com>
2025-02-02 21:42:20 +0900

    HOLO-203 <chore: kakao 타입 선언 interface에서 type으로 변경>

--- global.d.ts
+++ global.d.ts
@@ -1,34 +1,33 @@
-interface Kakao {
-    init: (apiKey: string) => void;
-    isInitialized: () => boolean;
-    Share: {
-      sendDefault: (params: {
-        objectType: "feed";
-        content: {
-          title: string;
-          description: string;
-          imageUrl: string;
-          link: {
-            mobileWebUrl: string;
-            webUrl: string;
-          };
+type Kakao = {
+  init: (apiKey: string) => void;
+  isInitialized: () => boolean;
+  Share: {
+    sendDefault: (params: {
+      objectType: "feed";
+      content: {
+        title: string;
+        description: string;
+        imageUrl: string;
+        link: {
+          mobileWebUrl: string;
+          webUrl: string;
         };
-        buttons: {
-          title: string;
-          link: {
-            mobileWebUrl: string;
-            webUrl: string;
-          };
-        }[];
-      }) => void;
-    };
+      };
+      buttons: {
+        title: string;
+        link: {
+          mobileWebUrl: string;
+          webUrl: string;
+        };
+      }[];
+    }) => void;
+  };
+};
+
+declare global {
+  interface Window {
+    Kakao: Kakao;
   }
-  
-  declare global {
-    interface Window {
-      Kakao: Kakao;
-    }
-  }
-  
-  export {}; // TypeScript에서 모듈로 인식하게 만듦
-  
+}
+
+export {}; // TypeScript에서 모듈로 인식하게 만듦
